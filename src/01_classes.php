aa79e87354f313cd13bbaa4c362df517ce4e8a4f
Sascha Wernars <s@scha.nl>
2022-07-19 00:13:11 +0200

    Finished episode 1+2: Classes & Objects

---
+++ src/01_classes.php
@@ -0,0 +1,16 @@
+<?php
+
+class Invoice{
+    
+}
+
+class AchevementBadge{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user){
+        
+    }
+    
+}
