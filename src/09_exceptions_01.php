bfb99d650d4bde663c2756ca226e536e71ac67ee
Sascha Wernars <s@scha.nl>
2022-07-20 23:30:11 +0200

    Finished Episode 9: Exceptions

---
+++ src/09_exceptions_01.php
@@ -0,0 +1,15 @@
+<?php
+
+function add($one, $two){
+
+    if(! is_float($one) || ! is_float($two)){
+        throw new InvalidArgumentException('Please provide a float.');
+    }
+    return $one + $two;
+};
+
+try {
+    echo add(2, []);
+} catch (InvalidArgumentException $e) {
+    echo 'Oh well.';
+}
